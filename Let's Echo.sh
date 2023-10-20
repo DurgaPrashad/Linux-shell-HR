98d3560109cb6e8d9cd66af817931913ffe35afa
Durga Prashad <120201162+DurgaPrashad@users.noreply.github.com>
2023-10-20 21:43:56 +0530

    Create Let's Echo.sh

---
+++ Let's Echo.sh
@@ -0,0 +1,30 @@
+# Let's Echo
+# Points: 111Rank: 6484
+# Problem
+
+# Write a bash script that prints the string "HELLO".
+
+# Input Format
+
+# There is no input file required for this problem.
+
+# Output Format
+
+# HELLO
+
+# Sample Input
+
+# -
+
+# Sample Output
+
+# HELLO
+
+# Explanation
+
+# -
+
+________________________________________________________________
+# This script prints "HELLO"
+echo "HELLO"
+
