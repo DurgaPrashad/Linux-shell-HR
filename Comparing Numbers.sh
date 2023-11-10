d09c4c1c422a9febf366b70a9c4956b986d45a39
Durga Prashad <120201162+DurgaPrashad@users.noreply.github.com>
2023-11-10 05:35:44 +0530

    Add files via upload

--- Comparing Numbers.sh
+++ Comparing Numbers.sh
@@ -1,59 +1,55 @@
-# Given two integers,  and , identify whether  or  or .
+# # Given two integers,  and , identify whether  or  or .
 
-# Exactly one of the following lines:
-# - X is less than Y
-# - X is greater than Y
-# - X is equal to Y
+# # Exactly one of the following lines:
+# # - X is less than Y
+# # - X is greater than Y
+# # - X is equal to Y
 
-# Input Format
+# # Input Format
 
-# Two lines containing one integer each ( and , respectively).
+# # Two lines containing one integer each ( and , respectively).
 
-# Constraints
+# # Constraints
+
+# # -
+
+# # Output Format
+
+# # Exactly one of the following lines:
+# # - X is less than Y
+# # - X is greater than Y
+# # - X is equal to Y
+
+# # Sample Input
+
+# # Sample Input 1
+
+# # 5  
+# # 2  
+# # Sample Input 2
+
+# # 2
+# # 2  
+# # Sample Input 3
+
+# # 2
+# # 3  
+# # Sample Output
+
+# # Sample Output 1
+
+# # X is greater than Y  
+# # Sample Output 2
+
+# # X is equal to Y   
+# # Sample Output 3
+
+# # X is less than Y  
+# # Explanation
 
 # -
 
-# Output Format
 
-# Exactly one of the following lines:
-# - X is less than Y
-# - X is greater than Y
-# - X is equal to Y
-
-# Sample Input
-
-# Sample Input 1
-
-# 5  
-# 2  
-# Sample Input 2
-
-# 2
-# 2  
-# Sample Input 3
-
-# 2
-# 3  
-# Sample Output
-
-# Sample Output 1
-
-# X is greater than Y  
-# Sample Output 2
-
-# X is equal to Y   
-# Sample Output 3
-
-# X is less than Y  
-# Explanation
-
-# -
-
-# Language: BASH
-
-
-
--------------------------------------------------------------------
 # Read the first integer (X)
 read X
 
